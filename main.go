ab2959c0861e43b9961030558eaf515f5a752d2a
Alex Ozer <thealexozer@gmail.com>
2013-12-24 10:55:07 -0500

    Add orthoCube sample

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"github.com/alexozer/opengl-samples-golang/tut02/fragPosition"
-	"github.com/alexozer/opengl-samples-golang/tut02/vertexColors"
-	"github.com/alexozer/opengl-samples-golang/tut03/cpuOffset"
-	"github.com/alexozer/opengl-samples-golang/tut03/fragChangeColor"
-	"github.com/alexozer/opengl-samples-golang/tut03/vertCalcOffset"
-	"github.com/alexozer/opengl-samples-golang/tut03/vertOffset"
+	//"github.com/alexozer/opengl-samples-golang/tut02/fragPosition"
+	//"github.com/alexozer/opengl-samples-golang/tut02/vertexColors"
+	//"github.com/alexozer/opengl-samples-golang/tut03/cpuOffset"
+	//"github.com/alexozer/opengl-samples-golang/tut03/fragChangeColor"
+	//"github.com/alexozer/opengl-samples-golang/tut03/vertCalcOffset"
+	//"github.com/alexozer/opengl-samples-golang/tut03/vertOffset"
+	"github.com/alexozer/opengl-samples-golang/tut04/orthoCube"
 )
 
 func main() {
-	fragPosition.Run()
-	vertexColors.Run()
-	cpuOffset.Run()
-	vertOffset.Run()
-	vertCalcOffset.Run()
-	fragChangeColor.Run()
+	//fragPosition.Run()
+	//vertexColors.Run()
+	//cpuOffset.Run()
+	//vertOffset.Run()
+	//vertCalcOffset.Run()
+	//fragChangeColor.Run()
+	orthoCube.Run()
 }
---
+++ tut04/orthoCube/orthoCube.go
@@ -0,0 +1,103 @@
+package orthoCube
+
+import (
+	"fmt"
+	"github.com/go-gl/gl"
+	glfw "github.com/go-gl/glfw3"
+	"github.com/go-gl/glh"
+
+	"github.com/alexozer/opengl-samples-golang/tut04"
+)
+
+func onError(err glfw.ErrorCode, desc string) {
+	fmt.Printf("%v: %v\n", err, desc)
+}
+
+func onKey(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	if key == glfw.KeyEscape && action == glfw.Press {
+		w.SetShouldClose(true)
+	}
+}
+
+func reshape(w *glfw.Window, width, height int) {
+	gl.Viewport(0, 0, width, height)
+}
+
+func Run() {
+	glfw.SetErrorCallback(onError)
+
+	if !glfw.Init() {
+		panic("Can't init glfw!")
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Graphics", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.SetKeyCallback(onKey)
+	window.SetSizeCallback(reshape)
+
+	window.MakeContextCurrent()
+	glfw.SwapInterval(1)
+
+	gl.Init()
+
+	gl.Enable(gl.CULL_FACE)
+	gl.CullFace(gl.BACK)
+	gl.FrontFace(gl.CW)
+
+	posBuffer = genVertexBuffer(tut04.VertexData)
+	shaderProgram = glh.NewProgram(vertShader, fragShader)
+	offsetLocation = shaderProgram.GetUniformLocation("offset")
+
+	for !window.ShouldClose() {
+		display()
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
+
+var (
+	posBuffer      gl.Buffer
+	shaderProgram  gl.Program
+	offsetLocation gl.UniformLocation
+)
+
+// All float32's have a size of 4 bytes
+const float32_size = 4
+const xOffset, yOffset float32 = 0.5, 0.25
+
+func genVertexBuffer(verts []float32) gl.Buffer {
+	buffer := gl.GenBuffer()
+	buffer.Bind(gl.ARRAY_BUFFER)
+	defer buffer.Unbind(gl.ARRAY_BUFFER)
+
+	gl.BufferData(gl.ARRAY_BUFFER, len(verts)*float32_size, verts, gl.STATIC_DRAW)
+
+	return buffer
+}
+
+func display() {
+	gl.ClearColor(0, 0, 0, 0)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+
+	shaderProgram.Use()
+	defer gl.ProgramUnuse()
+
+	offsetLocation.Uniform2f(xOffset, yOffset)
+
+	posBuffer.Bind(gl.ARRAY_BUFFER)
+
+	positionAttrib := gl.AttribLocation(shaderProgram.GetAttribLocation("position"))
+	positionAttrib.AttribPointer(4, gl.FLOAT, false, 0, uintptr(0))
+	positionAttrib.EnableArray()
+	defer positionAttrib.DisableArray()
+
+	colorAttrib := gl.AttribLocation(shaderProgram.GetAttribLocation("color"))
+	colorAttrib.AttribPointer(4, gl.FLOAT, false, 0, uintptr((len(tut04.VertexData)*float32_size)/2))
+	colorAttrib.EnableArray()
+	defer colorAttrib.DisableArray()
+
+	gl.DrawArrays(gl.TRIANGLES, 0, len(tut04.VertexData)/2/float32_size)
+}
